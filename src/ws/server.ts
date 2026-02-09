17d3b91bc6d72fa573eb1eb4f90cc92f7dfa76d0
Gabriel Kassarp <gkassarp@gmail.com>
2026-02-09 10:19:59 +0100

    installed missing dependency types

--- src/ws/server.ts
+++ src/ws/server.ts
@@ -1,26 +1,37 @@
-import [ WebSocketServer, WebSocket ] from 'ws';
+import { WebSocketServer, WebSocket } from "ws";
+import * as http from "http";
 
-function sendJson( socket: WebSocket, data: any ) {
-  if ( socket.readyState === WebSocket.OPEN ) {
-    socket.send( JSON.stringify( data ) );
+function sendJson(socket: WebSocket, data: any) {
+  if (socket.readyState === WebSocket.OPEN) {
+    socket.send(JSON.stringify(data));
   }
 }
 
-function broadcast( wss: WebSocket.Server, data: any ) {
-    for (const client of wss.clients) {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(data));
-        }
+function broadcast(wss: WebSocketServer, data: any) {
+  for (const client of wss.clients) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
     }
+  }
 }
 
-function attachWebSocketServer( server: http.Server ) {
-  const wss = new WebSocket.Server( { server, path: '/ws', maxPayload: 1024 * 1024 } );
-wss.on( 'connection', ( socket ) => {
-    sendJson( socket, { type: 'welcome', message: 'Welcome to the WebSocket server!' } );
+function attachWebSocketServer(server: http.Server) {
+  const wss = new WebSocketServer({
+    server,
+    path: "/ws",
+    maxPayload: 1024 * 1024,
+  });
+
+  wss.on("connection", (socket : WebSocket) => {
+    sendJson(socket, {
+      type: "welcome",
+      message: "Welcome to the WebSocket server!",
+    });
+  });
+
+  function broadcastMatchCreated(wss: WebSocketServer, match: any) {
+    broadcast(wss, { type: "matchCreated", match });
+  }
+
+  return { broadcastMatchCreated };
 }
-function broadcastMatchCreated( wss: WebSocket.Server, match: any ) {
-    broadcast( wss, { type: 'matchCreated', match } );
-}
-return { broadcastMatchCreated };
-});
